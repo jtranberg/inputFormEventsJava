8a57e6c096cc865c11bdfe22e89e05476b02f5bc
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-02 12:15:13 -0700

    refactoring to show form data and function to condense code

--- java/java.js
+++ java/java.js
@@ -11,3 +11,25 @@ form.addEventListener('submit', function(e){
  console.log('veggieSelect', veggieSelect.value);
 
 })
+const formData ={};
+creditCardInput.addEventListener('input', (e) => {
+    console.log('CC Changed', e);
+    formData['cc'] =e.target.value;
+});
+termsCheckbox.addEventListener('input', (e) => {
+    console.log('termsCheckbox', e);
+    formData['terms'] = e.target.checked;
+});
+veggieSelect.addEventListener('input', (e) => {
+    console.log('veggieSelect', e);
+    formData['veggieSelect'] =e.target.value;
+});
+
+for (let input of [creditCardInput, termsCheckbox, veggieSelect]) {
+    input.addEventListener('input', ({target}) => {
+     const {name, type, value, checked} = target;   
+        formData[name] = type === 'checkbox' ? checked : value;
+        console.log('formData');
+    
+    });
+}
